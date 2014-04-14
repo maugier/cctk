8960bf3e75701cd6be12e7f13805c42a33ab6b4b
Maxime Augier <max@xolus.net>
2014-04-14 14:10:48 +0200

    Enhance RSA API

--- RSA.hs
+++ RSA.hs
@@ -6,28 +6,37 @@ import Primes
 type Exponent = Integer
 type Modulus  = Integer
 type Prime    = Integer
+type DecExponent = Integer
 type Cleartext = Integer
 type Ciphertext = Integer
 
 type Key    = (Modulus, Exponent)
 type Params = (Prime, Prime)
 
-data PrivateKey = Private Key | Params Key Params
+data PrivateKey = PrivateKey (Maybe Modulus) Exponent (Maybe Prime) (Maybe Prime) (Maybe DecExponent)
 
 
 
+public :: PrivateKey -> Key
+public (PrivateKey (Just n) e) = (n,e)
+public p = public (computePrivate p)
+
+
+computePrivate :: PrivateKey -> PrivateKey
+computePrivate p(PrivateKey (Just _) _ (Just _) (Just _) (Just _) = p
+computePrivate (PrivateKey Nothing e (Just p) (Just q) d) = computePrivate (PrivateKey (Just (p*q)) e (Just p) (Just q) d)
+computePrivate (PrivateKey n e (Just p) (Just q) Nothing) = computePrivate n e (Just p) (Just q) (Just d) where
+    phi      = (p-1)*(q-1)
+    (_,d',_) = egcd phi e
+    d        = if d' > 0 then d' else d' + phi
 
 
 encrypt :: Key -> Cleartext -> Ciphertext
 encrypt (n,e) m = emod m e n
 
 decrypt :: PrivateKey -> Ciphertext -> Cleartext
-decrypt (Private (n,d)) c      = emod c d n
-decrypt (Params (n,e) (p,q)) c = decrypt (Private (n,d `mod` phi)) c where
-    phi     = (p-1)*(q-1)
-    (_,d,1) = egcd phi e
-
-square x = x*x
+decrypt (PrivateKey (Just n) _ _ _ (Just d) c      = emod c d n
+decrypt p c = decrypt (computePrivate p) c
 
 
 egcd a b = egcd' (1,0,a) (0,1,b) where
