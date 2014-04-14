5d9b620de937f827ca88de3d9ec9e88260071068
Maxime Augier <max@xolus.net>
2014-04-14 14:11:47 +0200

    Typo

--- RSA.hs
+++ RSA.hs
@@ -23,7 +23,7 @@ public p = public (computePrivate p)
 
 
 computePrivate :: PrivateKey -> PrivateKey
-computePrivate p(PrivateKey (Just _) _ (Just _) (Just _) (Just _) = p
+computePrivate p@(PrivateKey (Just _) _ (Just _) (Just _) (Just _) = p
 computePrivate (PrivateKey Nothing e (Just p) (Just q) d) = computePrivate (PrivateKey (Just (p*q)) e (Just p) (Just q) d)
 computePrivate (PrivateKey n e (Just p) (Just q) Nothing) = computePrivate n e (Just p) (Just q) (Just d) where
     phi      = (p-1)*(q-1)
