57a18a6d4bc829b2c8579ad32ae51c8360315a89
Maxime Augier <max@xolus.net>
2014-04-17 17:07:56 +0200

    Fix RSA

--- RSA.hs
+++ RSA.hs
@@ -14,18 +14,19 @@ type Key    = (Modulus, Exponent)
 type Params = (Prime, Prime)
 
 data PrivateKey = PrivateKey (Maybe Modulus) Exponent (Maybe Prime) (Maybe Prime) (Maybe DecExponent)
+	deriving Show
 
 
 
 public :: PrivateKey -> Key
-public (PrivateKey (Just n) e) = (n,e)
+public (PrivateKey (Just n) e _ _ _) = (n,e)
 public p = public (computePrivate p)
 
 
 computePrivate :: PrivateKey -> PrivateKey
-computePrivate p@(PrivateKey (Just _) _ (Just _) (Just _) (Just _) = p
+computePrivate p@(PrivateKey (Just _) _ (Just _) (Just _) (Just _)) = p
 computePrivate (PrivateKey Nothing e (Just p) (Just q) d) = computePrivate (PrivateKey (Just (p*q)) e (Just p) (Just q) d)
-computePrivate (PrivateKey n e (Just p) (Just q) Nothing) = computePrivate n e (Just p) (Just q) (Just d) where
+computePrivate (PrivateKey n e (Just p) (Just q) Nothing) = computePrivate (PrivateKey n e (Just p) (Just q) (Just d)) where
     phi      = (p-1)*(q-1)
     (_,d',_) = egcd phi e
     d        = if d' > 0 then d' else d' + phi
@@ -35,7 +36,7 @@ encrypt :: Key -> Cleartext -> Ciphertext
 encrypt (n,e) m = emod m e n
 
 decrypt :: PrivateKey -> Ciphertext -> Cleartext
-decrypt (PrivateKey (Just n) _ _ _ (Just d) c      = emod c d n
+decrypt (PrivateKey (Just n) _ _ _ (Just d)) c      = emod c d n
 decrypt p c = decrypt (computePrivate p) c
 
 
