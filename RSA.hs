f36853253350d827ba86b27d4f311b10ac3a3895
Maxime Augier <max@xolus.net>
2015-01-06 15:06:48 +0100

    Remove duplicate code

--- RSA.hs
+++ RSA.hs
@@ -28,7 +28,7 @@ computePrivate p@(PrivateKey (Just _) _ (Just _) (Just _) (Just _)) = p
 computePrivate (PrivateKey Nothing e (Just p) (Just q) d) = computePrivate (PrivateKey (Just (p*q)) e (Just p) (Just q) d)
 computePrivate (PrivateKey n e (Just p) (Just q) Nothing) = computePrivate (PrivateKey n e (Just p) (Just q) (Just d)) where
     phi      = (p-1)*(q-1)
-    (_,d',_) = egcd phi e
+    (_,d',1) = euclid phi e
     d        = if d' > 0 then d' else d' + phi
 
 
@@ -39,13 +39,5 @@ decrypt :: PrivateKey -> Ciphertext -> Cleartext
 decrypt (PrivateKey (Just n) _ _ _ (Just d)) c      = emod c d n
 decrypt p c = decrypt (computePrivate p) c
 
-
-egcd a b = egcd' (1,0,a) (0,1,b) where
-    egcd' r (_,_,0) = r
-    egcd' (x1,y1,g1) r2@(x2,y2,g2) = egcd' r2 (x3,y3,g3) where
-        m = g1 `div` g2
-        g3 = g1 - m*g2
-        x3 = x1 - m*x2
-        y3 = y1 - m*y2
 
 
