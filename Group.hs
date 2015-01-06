08e9a90d5ba0777cde655d42c681087d6bac5e61
Maxime Augier <max@xolus.net>
2015-01-06 16:12:47 +0100

    Bugfix in group, whoops

--- Group.hs
+++ Group.hs
@@ -9,8 +9,9 @@ class Group g where
 	inv   :: g -> g
 
 
+(|^|) :: Group g => g -> Integer -> g
 _ |^| 0 = i
 g |^| 1 = g
 g |^| n | n < 0     = inv $ g |^| (0-n)
 	| odd n     = g |*| (g |^| (n-1))
-        | otherwise = let g2 = (n `div` 2) |^| g in g2 <*> g2
+        | otherwise = let g2 = g |^| (n `div` 2) in g2 |*| g2
