0cedfacdcc6775b60e5565ba6f1c8e29fb360206
Maxime Augier <max@xolus.net>
2015-01-06 16:12:19 +0100

    Merge sorted lists per key

--- Merge.hs
+++ Merge.hs
@@ -1,5 +1,8 @@
 module Merge where
 
+import Data.List (sortBy)
+import Data.Ord (comparing)
+
 {- Merge sorted lists -}
 
 merge :: Ord k => [(k,a)] -> [(k,b)] -> [(k,a,b)]
@@ -8,3 +11,7 @@ merge aa@((ka,a):as) bb@((kb,b):bs) = case compare ka kb of
 	GT -> merge aa bs
 	EQ -> (ka,a,b) : merge as bs
 merge _ _ = []
+
+-- Try to factor this code. See how the typechecker bites you in the ass.
+merge' :: Ord k => [(k,a)] -> [(k,b)] -> [(k,a,b)]
+merge' as bs = merge (sortBy (comparing fst) as) (sortBy (comparing fst) bs)
