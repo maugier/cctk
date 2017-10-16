723c70edb3b8236d4a8645a3013575b1d8df016b
Maxime Augier <max@xolus.net>
2017-10-16 14:59:39 +0200

    Retab

--- Group.hs
+++ Group.hs
@@ -5,14 +5,16 @@ infixl 6 |/|
 infixl 7 |^|
 
 class Group g where
-	(|*|) :: g -> g -> g
-	i     :: g
-	inv   :: g -> g
-	(|^|) :: g -> Integer -> g
-	_ |^| 0 = i
-	g |^| 1 = g
-	g |^| n | n < 0     = inv $ g |^| (0-n)
-			| odd n     = g |*| (g |^| (n-1))
-			| otherwise = let g2 = g |^| (n `div` 2) in g2 |*| g2
+    (|*|) :: g -> g -> g
+    i     :: g
+    inv   :: g -> g
+    (|^|) :: g -> Integer -> g
+
+    _ |^| 0 = i
+    g |^| 1 = g
+    g |^| n | n < 0     = inv $ g |^| (0-n)
+            | odd n     = g |*| (g |^| (n-1))
+            | otherwise = let g2 = g |^| (n `div` 2) in g2 |*| g2
+
     (|/|) :: g -> g -> g
     a |/| b = a |*| inv b
