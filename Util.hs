39ad089607167b1d47ec5683779924da82219433
Maxime Augier <max@xolus.net>
2017-10-16 11:09:08 +0200

    Unhexlify

--- Util.hs
+++ Util.hs
@@ -1,9 +1,14 @@
 module Util where
 
-chunk n = map (take n) . takeWhile (not.null) . iterate (drop n)
+import qualified Data.ByteString as BS
 
+chunk n = map (take n) . takeWhile (not.null) . iterate (drop n)
 
 zipEither :: (b -> c -> d) -> [Either a b] -> [c] -> [Either a d]
 zipEither f (Left x  : xs) ys = Left x : zipEither f xs ys
 zipEither f (Right x : xs) (y:ys)  = Right (f x y) : zipEither f xs ys
 zipEither _ _ _ = []
+
+
+unhexlify :: String -> BS.ByteString
+unhexlify = BS.pack . map (read . ("0x"++)) . chunk 2
