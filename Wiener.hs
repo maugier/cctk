b4d898d1f2a1f1841bd70c9e0efc7c9500946883
Maxime Augier <max@xolus.net>
2017-10-16 11:08:50 +0200

    Wiener's attack on small decryption exponent for RSA

--- Wiener.hs
+++ Wiener.hs
@@ -1,11 +1,30 @@
 module Wiener where
 
 import CFrac
+import Data.Maybe
 import GHC.Real
+import Math.NumberTheory.Powers.Squares
 
--- wiener n e = (phi,d) such that ed = 1 mod phi(n)
-wiener :: Integer -> Integer -> Integer
-wiener n e = head [ phi | (k :% d) <- convergents (e % n)
+type Exponent = Integer
+type Modulus = Integer
+
+-- wiener n e = (p,q) such that p*q = n
+wiener :: Modulus -> Exponent -> (Integer, Integer)
+wiener n e = head [ (p,q) | (k :% d) <- convergents (e % n)
                         , k /= 0
-                        , let phi = (e*d - 1) `div` k
-                        , (e*d) `mod` phi == 1 ]
+                        , let (phi,r) = (e*d - 1) `divMod` k
+                        , r == 0
+                        , (e*d) `mod` phi == 1
+                        , (p,q) <- maybeToList $ factorWithPhi n phi
+                        , p*q == n
+                        ]
+
+
+factorWithPhi :: Integer -> Integer -> Maybe (Integer, Integer)
+factorWithPhi n phi = do
+    let b = n - phi + 1
+    let c = n
+    let d = b*b - 4*c
+    dr <- exactSquareRoot d
+    return ((b - dr) `div` 2, (b + dr) `div` 2)
+
