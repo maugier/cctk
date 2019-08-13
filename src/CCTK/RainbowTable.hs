6548c3aa9df7a82e4820301b6eda94e63203f973
Maxime Augier <max@xolus.net>
2019-08-13 16:26:34 +0200

    rainbow tables

--- src/CCTK/RainbowTable.hs
+++ src/CCTK/RainbowTable.hs
@@ -1,37 +1,30 @@
 module CCTK.RainbowTable (
     Table(),
     build,
-    crack
+    build1,
+    build',
+    crack,
+    shape,
+    effectiveness,
 ) where
 
 import CCTK.Digits
 
-import Data.List (foldl', tails)
+import Control.Monad.Random
+
+import Data.List (foldl', tails, unfoldr)
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Maybe
-import Data.Monoid
+import Data.Maybe (listToMaybe)
 
+import System.Random
+
+-- |- A Rainbow table for attacking a hash function of type `h -> c`
 data Table h c = Table {
     hash   :: c -> h,
     reduce :: [h -> c],
     table  :: !(Map h c)
 }
-
-{-
-c0 -> h0 -A> c1 -> h1 -B> c2 -> h2
-
-build : c0 -> h.chain $ c0
-
-attack: h0 -> h0, h1, h2
-
-attack h0 [] = h0
-attack h0 (r:rs) = attack (h (r h0)) rs
-
-attack = foldl' (flip $\r -> (h.r)))
-
-
--} 
 
 chain :: (c -> h) -> [h -> c] -> c -> c
 chain h = flip (foldl' (flip (.h)))
@@ -39,15 +32,43 @@ chain h = flip (foldl' (flip (.h)))
 attack :: (c -> h) -> [h -> c] -> h -> h
 attack h = flip (foldl' (flip (h.)))
 
+-- |- Check the shape of the table (colors, length)
+shape :: Table a b -> (Int,Int)
+shape (Table _ rs t) = (length rs + 1, M.size t)
 
+-- |- Attempt to crack a hash using the precomputed table
 crack :: Ord h => Table h c -> h -> Maybe c
 crack (Table h rs t) x =
-    getFirst $ mconcat [ First .Just $ chain h (take i rs) c 
-                       | (i,rs') <- zip [0..] (tails rs)
-                       , let y = (attack h rs') x
-                       , let (Just c) = M.lookup y t ]
+    listToMaybe [ c'
+                | (i,rs') <- zip [0..] (tails rs)
+                , let y = (attack h rs') x
+                , Just c <- [M.lookup y t]
+                , let c' = chain h (take i rs) c
+                , h c' == x ]
 
-build :: Ord h => (c -> h) -> Int -> c -> [h -> c] -> Table h c
-build h rows c (r:rs) = Table h rs (tab c M.empty) where
-    tab c t | M.size t >= rows = t
-            | otherwise        = let y = h (chain h rs c) in tab (r y) (M.insert y c t)
+-- |- Deterministically build a table for a given hash function.
+-- Takes a hash function to attack, a list of reducers, and a list of chain seeds.
+--
+-- If the list of reducers is empty, the table is equivalent to a full dictionary.
+build' :: Ord h => (c -> h) -> [h -> c] -> [c] -> Table h c
+build' h rs cs = Table h rs (M.fromList [ ((h . chain h rs) c ,c ) | c <- cs ])
+
+-- |- Iteratively build a table from a single seed.
+build1 :: Ord h => (c -> h) -> [h -> c] -> Int -> c -> Table h c
+build1 h (r:rs) rows = Table h rs . M.fromList . take rows . unfoldr step where
+    step c = let y = (h . chain h rs) c in Just ((y,c), r y)
+
+
+-- |- Build a table of the desired size from random seeds
+build :: (Ord h, Random c, MonadRandom m) => (c -> h) -> [h -> c] -> (Int, Int) -> m (Table h c)
+build = undefined
+
+
+effectiveness :: (Ord h, Random c) => Table h c -> IO Int
+effectiveness t = measure 100 0 where
+    measure 0 n = return n
+    measure k n = do
+        x <- getRandom
+        case crack t (hash t x) of
+            Just _ -> measure (k-1) (n+1)
+            _      -> measure (k-1) n
